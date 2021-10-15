486d14e95dfdde603073171f633998fb553ba2a6
Jonathan Durr <jmdurr@gmail.com>
2021-10-15 09:36:19 -0400

    add equals test

---
+++ testqualsignfolder=/inside/container_data_003/safeframe_002.js
@@ -0,0 +1,269 @@
+
+(function() {
+    // Dynamic values from page request
+    
+var script = document.getElementById("celtra-script-1");
+if (!script || script.tagName.toLowerCase() !== 'script')
+    throw 'Element with id equal to scriptId does not exist or is not a script.';
+  
+    var runtimeParams = {"tagVersion":"html-standard-7","deviceInfo":{"deviceType":"Desktop","primaryHardwareType":"desktop","mobileDevice":false,"osName":null,"osVersion":null,"platform":"DesktopPlatform","platformVersion":null,"browserName":"Firefox","browserVersion":"93.0","browserRenderingEngine":"Gecko","vendor":null,"model":null},"weather":{"windy":"0","currentCondition":"sunny","apparentTemperature":24.0,"temperature":24.6,"windSpeed":0,"cloudCoverage":0,"conditions":[{"name":"sunny","weight":1}]},"ipCountryCode":"US","ipRegionCode":null,"ipPrincipalSubdivisionCode":"VA","ipCityGeonameId":"4744870","ipCity":"Ashburn","ipPostalCode":"20149","ipLat":39.0469,"ipLng":-77.49031,"accountId":"d088b289","folderId":"cb417844","placementId":"e7692354","supplierId":null,"sessionId":"s1634247253x3bd6aab828bfb3x00852843","purpose":"live","secure":1,"clientTimestamp":"1634247253.237","clientTimeZoneOffsetInMinutes":240,"clientIp":"54.87.74.0","gpsLat":null,"gpsLng":null,"language":"en","acceptLanguage":"en-US,en;q=0.5","platformAdvId":null,"platformAdvIdTrackingLimited":null,"userIdentifiers":{},"variantChoices":{"EventMetadata":"newMeta"},"neustarSegment":null,"authBasis":"1634247253418,a21f5810,e7692354","authToken":"b600f4419433d70e22dfdcca4c266edc","customAudiences":{},"derivedAudiences":{},"destinationDefinitions":[{"campaignId":null,"placementId":null,"creativeId":"a21f5810","eventName":"urlOpened","reportLabel":"cta_click","destinationUrl":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l","destination":{"url":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l","clazz":"Fixed"},"impressionUrl":null},{"campaignId":null,"placementId":null,"creativeId":"a21f5810","eventName":"urlOpened","reportLabel":"cta_background","destinationUrl":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l","destination":{"url":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l","clazz":"Fixed"},"impressionUrl":null},{"campaignId":null,"placementId":null,"creativeId":"a21f5810","eventName":"urlOpened","reportLabel":"cta_logo","destinationUrl":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l","destination":{"url":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l","clazz":"Fixed"},"impressionUrl":null}],"dynamicContent":[],"tuneIosQueryStringFragment":null,"tuneAndroidQueryStringFragment":null,"tuneCustomAttributes":{},"moatCustomAttributes":{},"admarvel_format":null,"overrides":{"placementId":"e7692354","customAudiences":{},"derivedAudiences":{},"deviceInfo":{"deviceType":"Desktop","primaryHardwareType":"desktop","mobileDevice":false,"osName":null,"osVersion":null,"platform":"DesktopPlatform","platformVersion":null,"browserName":"Firefox","browserVersion":"93.0","browserRenderingEngine":"Gecko","vendor":null,"model":null},"language":"en","ipCountryCode":"US","ipRegionCode":null,"ipPrincipalSubdivisionCode":"VA","ipCityGeonameId":"4744870","ipCity":"Ashburn","weather":{"windy":"0","currentCondition":"sunny","apparentTemperature":24.0,"temperature":24.6,"windSpeed":0,"cloudCoverage":0,"conditions":[{"name":"sunny","weight":1}]},"clientTimestamp":"1634247253.237","clientTimeZoneOffsetInMinutes":240},"utSignals":null,"_mraidCheck":null,"fallbackSdk":null,"passthroughParameters":{},"googleMapsClientKey":"AIzaSyCgZ5dSjhwO79Xa8MFMD3WrS9qfk2ld2sU","shouldShowMapsPrompt":false,"pageSkinsHost":null,"pageSkinsPath":null,"stickToPageHeader":null,"externalAdServer":"DFPPremium","externalCreativeId":"138362987595","externalCreativeName":null,"externalPlacementId":"1579460094","externalPlacementName":null,"externalSiteId":"1543222014","externalSiteName":"thespruce.com","externalSupplierId":null,"externalSupplierName":null,"externalLineItemId":"5785073998","externalSessionId":null,"externalBundleId":null,"externalCreativeWidth":null,"externalCreativeHeight":null,"externalCampaignId":"2904243526","externalCampaignName":null,"externalAdvertiserId":"5065489806","externalDestinationUrl":null,"clickUrl":"https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjsvsdEv02JqPV5hd5_FGwSlQFPbfK4MG8iOani3WwOiIfbxV9lt7yKR18Jczp7PCLudbBBoUWCV-VKU7kHMMd7GSqhq1QXnN-zeWGgrJSQgE-VtGb_hD6LPUBLInURV8uJszlR18WYXpv8FCLW3gu8LpSYH32p2XNkrDvI8hq7mhdhPdwvk9u3OP956D2WyysvKmJVATUz-6ULLB9FEbeu1IZO7fZ5HVi2b4e6yFsk7M4oYTUrFUAP7hxfwO9ptzJYnYYa2O9tM13IfU9nPvrh-nf0AeFh2ptCw_Bz8dZ5MA4Z8hgFrfol_UCG3MS1W03Wb6DM6JTMd4py65hFykABk&sai=AMfl-YT97rLmyFjnfrmYVE770gho3rVoCPaW5e_OXYIqo7z3R-Hjsxr_rNEuuPlss10zmW4alkA2dm_Kyc0yxnEIPR_n6uz6ZZ1kvFo6XBWTfuy_4QJJzDD_58u1LQT7goEX&sig=Cg0ArKJSzNw0yVP8clMvEAE&fbs_aeid=[gw_fbsaeid]&urlfix=1&adurl=","useClickAsDestination":null,"scriptId":"celtra-script-1","firehoseUrl":null,"clickEvent":"advertiser","clickUrlNeedsDest":null,"ncu":null,"firstPage":1,"dataURIsEnabled":0,"monotypeProjectId":"c46ed090-3671-4163-a85b-b06b4038ae38","iosAdvId":null,"iosAdvIdTrackingLimited":null,"androidAdvId":null,"androidAdvIdTrackingLimited":null,"moatRandom":{"first":141534531,"second":1285486865},"skipOffset":null,"_enablePoliteLoading":null,"massProductionPreview":null,"fontSubsetterIsDisabled":null,"_politeImageUrl":null,"_politeClickThrough":null,"sticky":null,"_mopubExt":null,"enabledServices":["googleMaps","contentSharing"],"creativeVariantLockSize":null,"screenIdLock":null,"vastVersion":null,"standalonePreview":null,"campaignExplorer":null,"exportingFirstFrame":null,"exportingFrameRate":null,"exportingVideoAssetBlobHash":null,"country":"US","productCategoryCode":null,"campaignName":"Delta Airlines_Q4 Sustainability_120983","agencyId":null,"customPartnerAttributeBrandId":null,"suppressCloseButton":null,"coppa":"0","expandDirection":"","hostPageLoadId":"7055176729449137"};
+    runtimeParams.overridableClickThroughDestinationUrl = false;
+    runtimeParams.redirectJsClientTimestamp = new Date() / 1000;
+    
+    
+    
+var macros = function (x) {
+    if (x instanceof Array) {
+        return x.map(macros);
+    } else {
+        var macroTags = [
+            ['%%CACHEBUSTER%%', (Math.random()+'').slice(2)]
+,['%ecid!', "138362987595"]
+,['%n', (Math.random()+'').slice(2)]
+,['%s', "https"]
+,['\\{celtraAccountId\\}', "d088b289"]
+,['\\{celtraAgencyId\\}', ""]
+,['\\{celtraAndroidAdvIdTrackingLimitedBoolStr\\}', ""]
+,['\\{celtraAndroidAdvIdTrackingLimited\\}', ""]
+,['\\{celtraAndroidAdvId\\}', ""]
+,['\\{celtraCampaignId:int\\}', "3410065476"]
+,['\\{celtraCampaignId\\}', "cb417844"]
+,['\\{celtraCampaignName\\}', "Delta%20Airlines_Q4%20Sustainability_120983"]
+,['\\{celtraCountryCode\\}', "US"]
+,['\\{celtraCreativeId:int\\}', "2719963152"]
+,['\\{celtraCreativeId\\}', "a21f5810"]
+,['\\{celtraCreativeVariant:urlenc\\}', ""]
+,['\\{celtraCreativeVariant\\}', ""]
+,['\\{celtraCustomPartnerAttribute\\[code\\]\\}', ""]
+,['\\{celtraExternalAdServer\\}', "DFPPremium"]
+,['\\{celtraExternalAdvertiserId\\}', "5065489806"]
+,['\\{celtraExternalBundleId\\}', ""]
+,['\\{celtraExternalCampaignId\\}', "2904243526"]
+,['\\{celtraExternalCampaignName\\}', ""]
+,['\\{celtraExternalCreativeId\\}', "138362987595"]
+,['\\{celtraExternalCreativeName\\}', ""]
+,['\\{celtraExternalDestinationUrl\\}', ""]
+,['\\{celtraExternalLineItemId\\}', "5785073998"]
+,['\\{celtraExternalPlacementId\\}', "1579460094"]
+,['\\{celtraExternalPlacementName\\}', ""]
+,['\\{celtraExternalSessionId\\}', ""]
+,['\\{celtraExternalSiteId\\}', "1543222014"]
+,['\\{celtraExternalSiteName\\}', "thespruce.com"]
+,['\\{celtraExternalSupplierId\\}', ""]
+,['\\{celtraExternalSupplierName\\}', ""]
+,['\\{celtraFeedReportLabel\\}', ""]
+,['\\{celtraIosAdvIdTrackingLimitedBoolStr\\}', ""]
+,['\\{celtraIosAdvIdTrackingLimited\\}', ""]
+,['\\{celtraIosAdvId\\}', ""]
+,['\\{celtraPlacementId:int\\}', "3882427220"]
+,['\\{celtraPlacementId\\}', "e7692354"]
+,['\\{celtraPlatformAdvIdTrackingLimited\\}', ""]
+,['\\{celtraPlatformAdvId\\}', ""]
+,['\\{celtraProductCategoryCode\\}', ""]
+,['\\{celtraProto\\}', "https"]
+,['\\{celtraRandom\\}', (Math.random()+'').slice(2)]
+,['\\{celtraSessionId\\}', "s1634247253x3bd6aab828bfb3x00852843"]
+,['\\{celtraSupplierId:int\\}', ""]
+,['\\{celtraSupplierId\\}', ""]
+
+        ];
+        return macroTags.reduce(function(str, replacementRule, idx, arr) {
+            return str.replace(new RegExp(replacementRule[0], 'ig'), replacementRule[1] ? replacementRule[1] : '');
+        }, x);
+    }
+};
+
+    // Dynamic values that we do not want to pass forward in urls,
+    // so we look them up on every page request based on runtimeParams
+    var urlOpenedOverrideUrls = {"cta_click":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l","cta_background":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l","cta_logo":"https://ad.doubleclick.net/ddm/clk/505674672;312641845;l"};
+    var storeOpenedOverrideUrls = {};
+    var urlOpenedUrlAppendage = "";
+    var clickThroughDestinationUrl = null;
+
+    // Less dynamic values for payload request
+    var payloadBase = "https://cache-ssl.celtra.com/api/creatives/a21f5810/compiled/safeframe.js";
+    var cacheParams = {"v": "4-3f9204adaf", "secure": 1, "cachedVariantChoices": "W10-", "isPurposePreview": 0, "eventMetadataExperiment": 'newMeta', "inmobi": typeof imraid !== 'undefined' && typeof _im_imai !== 'undefined' ? '1' : '0', "adx-in-banner-video": '1'};
+
+    
+    var trackers = (function() {
+    return [
+        // 3rd-party tracker (regular)
+function(event) {
+    if (event.name == 'adLoading')
+        return {urls: macros([])};
+
+    if (event.name == 'firstInteraction')
+        return {urls: macros([])};
+
+    if (event.name == 'creativeLoaded')
+        return {urls: macros([])};
+
+    if (event.name == 'creativeRendered')
+        return {urls: macros([])};
+
+    if (event.name == 'viewable00')
+        return {urls: macros([])};
+
+    if (event.name == 'viewable501')
+        return {urls: macros([])};
+
+    if (event.name == 'expandRequested')
+        return {urls: macros([])};
+
+    if (event.name == 'videoPlayInitiated')
+        return {urls: macros([])};
+
+    if (event.clazz === "MasterVideo") {
+        if (event.name == 'videoStart')
+            return {urls: macros([])};
+
+        if (event.name == 'videoFirstQuartile')
+            return {urls: macros([])};
+
+        if (event.name == 'videoMidpoint')
+            return {urls: macros([])};
+
+        if (event.name == 'videoThirdQuartile')
+            return {urls: macros([])};
+
+        if (event.name == 'videoComplete')
+            return {urls: macros([])};
+    }
+
+    if (event.name == 'videoPause')
+        return {urls: macros([])};
+
+    if (event.name == 'videoMuted')
+        return {urls: macros([])};
+
+    if (event.name == 'videoUnmuted')
+        return {urls: macros([])};
+
+    if (event.name == 'custom')
+        return {urls: macros({}[event.label] || [])};
+
+    if (event.name == 'urlOpened')
+        return {urls: macros({}[event.label] || [])};
+
+    if (event.name == 'storeOpened')
+        return {urls: macros({}[event.label] || [])};
+},
+// 3rd-party tracker (click regular)
+function(event) {
+    if (event.name === "urlOpened" && event.label === "cta_background" || event.name === "urlOpened" && event.label === "cta_click" || event.name === "urlOpened" && event.label === "cta_logo")
+        return {urls: macros([]), events: [{name: 'click'}] };
+},
+// Ad server tracker
+function(event) {
+    if (event.name === "urlOpened" && event.label === "cta_background" || event.name === "urlOpened" && event.label === "cta_click" || event.name === "urlOpened" && event.label === "cta_logo")
+        return {urls: macros(["https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjsvsdEv02JqPV5hd5_FGwSlQFPbfK4MG8iOani3WwOiIfbxV9lt7yKR18Jczp7PCLudbBBoUWCV-VKU7kHMMd7GSqhq1QXnN-zeWGgrJSQgE-VtGb_hD6LPUBLInURV8uJszlR18WYXpv8FCLW3gu8LpSYH32p2XNkrDvI8hq7mhdhPdwvk9u3OP956D2WyysvKmJVATUz-6ULLB9FEbeu1IZO7fZ5HVi2b4e6yFsk7M4oYTUrFUAP7hxfwO9ptzJYnYYa2O9tM13IfU9nPvrh-nf0AeFh2ptCw_Bz8dZ5MA4Z8hgFrfol_UCG3MS1W03Wb6DM6JTMd4py65hFykABk&sai=AMfl-YT97rLmyFjnfrmYVE770gho3rVoCPaW5e_OXYIqo7z3R-Hjsxr_rNEuuPlss10zmW4alkA2dm_Kyc0yxnEIPR_n6uz6ZZ1kvFo6XBWTfuy_4QJJzDD_58u1LQT7goEX&sig=Cg0ArKJSzNw0yVP8clMvEAE&fbs_aeid=[gw_fbsaeid]&urlfix=1&adurl="]), events: [{name: 'clickReportedToSupplier'}] };
+}
+    ]
+})();
+    trackers.urlsAndEventsFor = function(event) {
+        return this.reduce(function(acc, tracker) {
+            var ue = tracker(event) || {};
+            return {
+                urls:   acc.urls.concat(ue.urls || []),
+                events: acc.events.concat(ue.events || [])
+            };
+        }, {urls: [], events: []});
+    };
+       
+
+    
+var adLoadingEvent = {"name":"adLoading","sessionId":"s1634247253x3bd6aab828bfb3x00852843"};
+adLoadingEvent.clientTimestamp = new Date/1000;
+
+trackers.urlsAndEventsFor(adLoadingEvent).urls.forEach(function(url) {
+    // On IE 8+ URLs containing '%' character sometimes throw an error and
+    // stop current JS run loop. One solution  would be to look for that
+    // and replace it with '%25', but we've decided not to modify incoming
+    // URLs, because this issue is really a combination of two external
+    // problems: broken URL on a buggy browser.
+    // https://celtra.atlassian.net/browse/MAB-4476
+    try {
+        var img = new Image;
+        
+        img.src = url;
+    } catch(e) {}
+});
+    
+
+    
+(function () {
+    runtimeParams.protoLoading = {};
+
+    var base64img = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4wEKCBsN103sxwAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAC0lEQVQI12NgAAIAAAUAAeImBZsAAAAASUVORK5CYII=";
+
+    runtimeParams.protoLoading.dataLoadStatus = null;
+    // Test if CSP is blocking "data:" source for images
+    var dataImg = new Image();
+    dataImg.onload = function() {
+        runtimeParams.protoLoading.dataLoadStatus = "supported";
+    };
+    dataImg.onerror = function(e) {
+        runtimeParams.protoLoading.dataLoadStatus = "blocked";
+    };
+    dataImg.src = "data:image/png;base64," + base64img;
+
+    runtimeParams.protoLoading.blobLoadStatus = null;
+    // Test if CSP is blocking "blob:" source for images
+    var url = null;
+    try {
+        var binaryImg = atob(base64img);
+        var length = binaryImg.length;
+        var ab = new ArrayBuffer(length);
+        var ua = new Uint8Array(ab);
+        for (var i = 0; i < length; i++) {
+            ua[i] = binaryImg.charCodeAt(i);
+        }
+        var blob = new Blob([ab], {type: 'image/png'});
+        url = URL.createObjectURL(blob);
+    } catch(error) {
+        runtimeParams.protoLoading.blobLoadStatus = "error";
+        runtimeParams.protoLoading.blobErrorMessage = error.name + ': ' + error.message;
+        return;
+    }
+    var blobImg = new Image();
+    blobImg.onload = function() {
+        runtimeParams.protoLoading.blobLoadStatus = "supported";
+        URL.revokeObjectURL(url);
+    };
+    blobImg.onerror = function(e) {
+        runtimeParams.protoLoading.blobLoadStatus = "blocked";
+    };
+    blobImg.src = url;
+})();
+    
+
+    function buildPayloadUrl(payloadBase) {
+      var pairs = [];
+      for (var k in cacheParams)
+          pairs.push(encodeURIComponent(k) + '=' + encodeURIComponent(cacheParams[k]));
+      return payloadBase + '?' + pairs.join('&');
+    }
+
+    var payloadUrl = buildPayloadUrl(payloadBase);
+
+    
+// Request and run payload
+var payload = document.createElement('script');
+payload.src = payloadUrl;
+payload.onload = function() {
+
+runtimeParams.payloadJsClientTimestamp = new Date() / 1000;
+window.celtraDeviceInfoRuntimeParams = runtimeParams.deviceInfo;
+window.celtra.payloads[payloadUrl](script, runtimeParams, trackers, urlOpenedOverrideUrls, storeOpenedOverrideUrls, macros, urlOpenedUrlAppendage, clickThroughDestinationUrl);
+    
+};
+script.parentNode.insertBefore(payload, script.nextSibling);
+    
+    
+// Set creative height
+document.body.style.minHeight = '250px';
+      
+    
+    
+})();
+  
